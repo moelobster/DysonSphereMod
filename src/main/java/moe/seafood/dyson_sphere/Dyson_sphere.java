e985749b4909cd21e10040cf49acec05ff94d965
moe_lobster <2211479860@qq.com>
2024-10-01 14:05:53 +0800

    init

--- src/main/java/moe/seafood/dyson_sphere/Dyson_sphere.java
+++ src/main/java/moe/seafood/dyson_sphere/Dyson_sphere.java
@@ -1,10 +1,15 @@
 package moe.seafood.dyson_sphere;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
 
 public class Dyson_sphere implements ModInitializer {
-
+    public static final String MOD_ID = "dyson_sphere";
     @Override
     public void onInitialize() {
+    }
+    public static @NotNull ResourceLocation of(String path) {
+        return new ResourceLocation(MOD_ID, path);
     }
 }
---
+++ src/main/java/moe/seafood/dyson_sphere/data/Dyson_sphereDatagen.java
@@ -0,0 +1,11 @@
+package moe.seafood.dyson_sphere.data;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Dyson_sphereDatagen implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
