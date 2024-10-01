5cd5cd5f0896393bbc460bb21c833c5ef40790a7
moe_lobster <2211479860@qq.com>
2024-10-01 13:50:10 +0800

    init

---
+++ src/main/java/moe/seafood/dyson_sphere/Dyson_sphere.java
@@ -0,0 +1,10 @@
+package moe.seafood.dyson_sphere;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Dyson_sphere implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/moe/seafood/dyson_sphere/client/Dyson_sphereClient.java
@@ -0,0 +1,10 @@
+package moe.seafood.dyson_sphere.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Dyson_sphereClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
